2f167cee74ade36d3ffa733289c8743559435702
Marcos Blánquez <marcosblanquez9@gmail.com>
2025-03-26 11:34:38 +0100

    Feat: Añadida la libreria para conectar mysql, y algunos atributos de vehiculos

--- ERP para talleres/src/Vehiculos.java
+++ ERP para talleres/src/Vehiculos.java
@@ -1,3 +1,17 @@
+
+
 public class Vehiculos {
+
+    private String matricula;
+    private String marca;
+    private String modelo;
+
+
+    public Vehiculos (String matricula, String marca, String modelo) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+    
 
 }
