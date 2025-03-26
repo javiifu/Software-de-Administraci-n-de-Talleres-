3f5de621776bb54aa4b60c3b23ee7ca8abd28970
Javiifu <javibeltra89@gmail.com>
2025-03-26 12:32:20 +0100

    Creación de submenus dentro de Taller.java y modificacciones en App.java

--- ERP para talleres/src/App.java
+++ ERP para talleres/src/App.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Taller taller = new Taller();
         Scanner sc = new Scanner(System.in);
         int opcion;
         System.out.println("Hola bienvenido a ERP para talleres que hemos creado Marcos Blanquez y Javier Beltrán ");
@@ -12,9 +13,9 @@ public class App {
             System.out.println("1. Registros.");
             System.out.println("2. Servicios.");
             System.out.println("3. Inventario.");
-            System.out.println("4. Asiganar empleado a Servicio.");
+            System.out.println("4. Facturas.");
             System.out.println("5. Citas.");
-            System.out.println("6. Facturas.");
+            System.out.println("6. Asiganar empleado a Servicio");
             System.out.println("7. Listas (Empleados, Clientes, Servicios y Proveedores).");
             System.out.println("8. Gestionar (Proveedores, Empleados, Clientes).");
             System.out.println("9. Contabilidad.");
@@ -22,7 +23,11 @@ public class App {
             opcion = sc.nextInt();
             sc.nextLine(); //Limpiamos el bufer del scanner
         } while (opcion != 10);
-
+        
+        switch(opcion){
+            case 1 -> taller.menuRegistro();
+            case 2 -> taller.menuServicios();
+        }
         sc.close();
     }
 }
--- ERP para talleres/src/Taller.java
+++ ERP para talleres/src/Taller.java
@@ -2,4 +2,73 @@ import java.util.Scanner;
 
 public class Taller {
 
+    Scanner sc = new Scanner(System.in);
+    int opcion;
+    //sub Menu de Registro
+    public void menuRegistro(){
+        
+        
+        do{
+            System.out.println("¿Qué desea registrar? (Escriba el número asociado a la opción):");
+            System.out.println("1. Registrar Cliente.");
+            System.out.println("2. Registrar Empleado.");
+            System.out.println("3. Registrar Proveedor.");
+            System.out.println("4. Volver al menú principal.");
+            opcion = sc.nextInt();
+            sc.nextLine(); //Todos los sc.nextLine() o sc.next() serán para limpiar el bufer del scanner
+        } while (opcion !=4);
+
+    }
+    //sub Menu para servicios
+    public void menuServicios(){
+
+        do { 
+            System.out.println("¿Qué desea hacer? (Escriba el número asociado a la opción):");
+            System.out.println("1. Crear Servicio.");
+            System.out.println("2. Modificar Servicio.");
+            System.out.println("3. Eliminar Servicio.");
+            System.out.println("4. Volver al menú principal.");
+            opcion = sc.nextInt();
+            sc.nextLine(); 
+        } while (opcion != 4);
+    }
+    //sub Menu para Inventario
+    public void menuInventario(){
+
+        do { 
+            System.out.println("¿Qué desea hacer? (Escriba el número asociado a la opción):");
+            System.out.println("1. Añadir Producto.");
+            System.out.println("2. Modificar Producto.");
+            System.out.println("3. Eliminar Producto.");
+            System.out.println("4. Volver al menú principal.");
+            opcion = sc.nextInt();
+            sc.nextLine(); 
+        } while (opcion != 4);
+    }
+    //sub Menu para gestionar las facturas
+    public void menuFacturas(){
+        do { 
+            System.out.pritnln("¿Qué desea hacer? (Escriba el número asociado a la opción):");
+            System.out.println("1. Crear Factura.");
+            System.out.println("2. Modificar Factura.");
+            System.out.println("3. Mostrar Factura específica.");
+            System.out.println("4. Volver al menú principal.");
+            opcion = sc.nextInt();
+            sc.nextLine();
+        } while (opcion != 4);
+    }
+
+    //sub Menu para gestionar las citas
+    public void menuCitas(){
+        do { 
+            System.out.println("¿Qué desea hacer? (Escriba el número asociado a la opción):");
+            System.out.println("1. Crear cita.");
+            System.out.println("2. Modificar cita.");
+            System.out.println("3. Eliminar cita.");
+            System.out.println("4. Volver al menú principal.");
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+        } while (opcion !=4);
+    }
 }
